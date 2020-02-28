c353c86edcc53431da80ee3fecd9b356997a581e
hujinhao <hujinhao25@163.com>
2020-02-28 21:57:59 +0800

    define print hello function

--- hellow.c
+++ hellow.c
@@ -1,36 +1,4 @@
-#include<stdio.h>
-
-uint8 main(void)
+void printHello(void)
 {
-   printf("%s","hello master");
-   return 0;
+   printf("%s","hello.");
 }
-
-uint8 master(void)
-{
-   print branch
-   return 0;
-<<<<<<< HEAD
-   
-   
-   branch2
-   branch2
-}
-=======
-}
-
-uint8 master1(void)
-{
-   print branch
-   return 0;
-}
-
-remote
-remote
-remote
-<<<<<<< HEAD
-
-0509
-=======
->>>>>>> hujinhao25@163.com/feature
->>>>>>> 125d54a5053a836b1da0cd8991f98cb15a1d703f
